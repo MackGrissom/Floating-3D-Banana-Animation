acb68e438237ba0a2f4a36f11a2582e98a643a31
Mack Grissom <mcintyregrissom@gmail.com>
2023-01-15 14:51:29 -0500

    finished box animations - ready to import models

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import * as THREE from 'three'
 
 function Box({z}) {
 const ref = useRef()
-const { viewport} = useThree()
+const { viewport, camera} = useThree()
+const {width, height} = viewport.getCurrentViewport(camera, [0,0,-10])
+
 
 const [data] = useState({
   x: THREE.MathUtils.randFloatSpread(2),
-  y:THREE.MathUtils.randFloatSpread(viewport.height),
+  y:THREE.MathUtils.randFloatSpread(height),
 })
 
   useFrame((state) => {
-    ref.current.position.set(data.x * viewport.width,(data.y += 0.1),0)
-    if (data.y > viewport.height / 1.5) {
-      data.y = -viewport.height / 1.5
+    ref.current.position.set(data.x * width,(data.y += 0.5), z)
+    if (data.y > height / 1.5) {
+      data.y = -height / 1.5
     }
 
   })
@@ -32,7 +34,7 @@ const [data] = useState({
 export default function App({count = 100}) {
   return ( 
   <Canvas>
-    {Array.from({length: count}, (_, i) => (<Box key={ i } z={i}/>))}
+    {Array.from({length: count}, (_, i) => (<Box key={ i } z={-i}/>))}
   </Canvas>
 )
 }  
